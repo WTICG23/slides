f334eaee6dd01fd8dd49ca3d0f07a957621599f8
K D <diego.unipampa@gmail.com>
2023-09-01 20:20:56 -0300

    updates

--- gera_lista.sh
+++ gera_lista.sh
@@ -16,7 +16,7 @@ do
     NAME=$(basename $FILE .pdf)
     if [ -f arquivos/$NAME.txt ]
     then
-        echo "[[Online no Google Drive] $NAME]($(cat arquivos/$NAME.txt))"
+        echo "[[Online no Google Drive] $NAME]($(cat arquivos/$NAME.txt))(faça uma cópia e edite no seu Google Drive! ;-))"
     elif [ -f arquivos/$NAME.pptx ]
     then
         echo "[[PPTX] $NAME.pptx]($URL/arquivos/$NAME.pptx)"
